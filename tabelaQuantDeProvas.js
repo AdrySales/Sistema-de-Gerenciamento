8c14c9f5c36b3c4bbb2b7ffe76df33545b3b665b
adrySales <yasmim.adrieny@gmail.com>
2024-05-17 07:05:52 -0300

    sistema

--- tabelaQuantDeProvas.js
+++ tabelaQuantDeProvas.js
@@ -110,7 +110,7 @@ async function obterQuantidadeDeLinhasRegionalDia(regional) {
     const mes = String(hoje.getMonth() + 1).padStart(2, '0');
     const dia = String(hoje.getDate()).padStart(2, '0');
     const dataHoje = `${ano}-${mes}-${dia}`;
-    const resultDia = await client.query("SELECT COUNT(id), external_id FROM (SELECT DISTINCT ea.id, ar.external_id FROM evaluation_applications ea INNER JOIN community_rooms_users cru ON ea.user_id = cru.user_id INNER JOIN community_rooms cr ON cru.room_id = cr.id INNER JOIN access_regions ar ON cr.region_id = ar.id WHERE ea.list_id IN (4281, 4265, 4267, 4282, 4285, 4289, 4291, 4293, 4306, 4290) AND ar.external_id = $1 AND ea.end IS NOT NULL AND ea.end <= $3 AND ea.start >= $2 AND ea.deleted_at IS NULL AND cru.deleted_at IS NULL AND cr.deleted_at IS NULL AND ar.deleted_at is NULL) AS unique_rows GROUP BY external_id", [regional, dataHoje + ' 00:00:00', dataHoje + ' 23:59:00']);       
+    const resultDia = await client.query("SELECT COUNT(id), external_id FROM (SELECT DISTINCT ea.id, ar.external_id FROM evaluation_applications ea INNER JOIN community_rooms_users cru ON ea.user_id = cru.user_id INNER JOIN community_rooms cr ON cru.room_id = cr.id INNER JOIN access_regions ar ON cr.region_id = ar.id WHERE ea.list_id IN (4281, 4265, 4267, 4282, 4285, 4289, 4291, 4293, 4306, 4290) AND ar.external_id = $1 AND ea.end IS NOT NULL AND ea.end <= $3 AND ea.start >= $2 AND ea.deleted_at IS NULL AND cru.deleted_at IS NULL AND cr.deleted_at IS NULL AND ar.deleted_at is NULL) AS unique_rows GROUP BY external_id", [regional, dataHoje + ' 00:00:01', dataHoje + ' 23:59:00']);       
     if (resultDia.rows.length === 0) {
       return 0; // or any default value you prefer
     }
